76b2ea1d573443af81732b00ad3658eee08ebefc
Paul De Ulloa <pauld143@gmail.com>
2023-09-29 00:41:50 -0700

    edits made to both files and if else added

--- BankAccount.java
+++ BankAccount.java
@@ -1,32 +1,32 @@
 public class BankAccount {
     // MEMBER VARIABLES
     private double checkingBalance;
-    private double savingsBalance;
-    private static int accounts;
-    private static double totalMoney; 
-    // ^ refers to the sum of all bank account checking and savings balances
+    private double savingBalance;
+    private static int accounts = 0;
+    private static double totalMoney = 0; 
+    // ^ refers to the sum of all bank account checking and saving balances
 
     // CONSTRUCTOR
     // Be sure to increment the number of accounts
     public BankAccount(){
         this.checkingBalance = 0;
-        this.savingsBalance = 0;
+        this.savingBalance = 0;
     }
 
 
 
     // GETTERS
-    // for checking, savings, accounts, and totalMoney
+    // for checking, saving, accounts, and totalMoney
     public double getCheckingBalance(){
         return this.checkingBalance;
     }
     public double getSavingBalance(){
-        return this.savingsBalance;
+        return this.savingBalance;
     }
-    public static getAccounts(){
+    public static int getAccounts(){
         return this.accounts;
     }
-    public static getTotalMoney(){
+    public static double getTotalMoney(){
         return this.totalMoney;
     }
 
@@ -34,7 +34,7 @@ public class BankAccount {
 
     // METHODS
     // deposit
-    // - users should be able to deposit money into their checking or savings account
+    // - users should be able to deposit money into their checking or saving account
     public void deposit(double deposit){
         this.checkingBalance += deposit;
         totalMoney += deposit;
@@ -49,25 +49,30 @@ public class BankAccount {
 
 
     // withdraw 
-    // - users should be able to withdraw money from their checking or savings account
+    // - users should be able to withdraw money from their checking or saving account
     // - do not allow them to withdraw money if there are insufficient funds
     // - all deposits and withdrawals should affect totalMoney
-    public void withdraw(double outgoingMoney){
-        if(outgoingMoney <= this.savingsBalance){
-            this.checkingBalance -= outgoingMoney;
-            totalMoney -= outgoingMoney;
-        };
+    public void withdraw(double withdraw){
+        if(this.checkingBalance < withdraw){
+            System.out.println("Insufficient funds")
+        }else{
+            this.checkingBalance -= withdraw;
+            totalMoney -= withdraw;
+        }
     }
-    public void withdraw(double outgoingMoney){
-        if(outgoingMoney <= this.savingsBalance){
-            this.savingsBalance -= outgoingMoney;
-            totalMoney -= outgoingMoney;
-        };
+    public void withdraw(double withdraw){
+        if(this.savingBalance < withdraw){
+            System.out.println("Insufficient funds")
+        }else{
+            this.savingBalance -= withdraw;
+            totalMoney -= withdraw;
+        }
     }
-
 
     // getBalance
-    // - display total balance for checking and savings of a particular bank account
-
+    // - display total balance for checking and saving of a particular bank account
+    public double getBalance(){
+        return checkingBalance + savingBalance;
+    }
 
 }
--- BankTest.java
+++ BankTest.java
@@ -4,25 +4,33 @@ public class BankTest {
 
         System.out.println("Total Accounts: " + BankAccount.getAccounts());
         //* ^gives us current count before going through the listed below accounts
-        
+
         BankAccount account1 = new BankAccount();
         BankAccount account2 = new BankAccount();
         BankAccount account3 = new BankAccount();
 
-        System.out.println("Total Accounts: " + BankAccount.getAccounts());
 
 
         // Deposit Test
         // - deposit some money into each bank account's checking or savings account and display the balance each time
         // - each deposit should increase the amount of totalMoney
-
-
-
-
+        account1.deposit(100);
+        account2.deposit(300);
+        account3.deposit(467); 
+        System.out.println("account1 Money: " + account1.getCheckingBalance());
+        System.out.println("account2 Money: " + account2.getCheckingBalance());
+        
 
         // Withdrawal Test
         // - withdraw some money from each bank account's checking or savings account and display the remaining balance
         // - each withdrawal should decrease the amount of totalMoney
+
+        account1.withdraw(50);
+        account2.withdraw(40);
+        account3.withdraw(67); 
+
+        System.out.println("account1 Money: " + account1.getCheckingBalance());
+        System.out.println("account2 Money: " + account2.getCheckingBalance());
 
 
 
@@ -32,5 +40,7 @@ public class BankTest {
 
         // Static Test (print the number of bank accounts and the totalMoney)
 
+        System.out.println("Total Accounts: " + BankAccount.getAccounts());
+        System.out.println("Total Money: " + BankAccount.getTotalMoney());
     }
 }
